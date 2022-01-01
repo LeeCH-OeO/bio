baca1c6b902a05f150580f0467527730becb730b
LeeCH-OeO <fairprice81000@gmail.com>
2022-01-02 02:31:04 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <img className='pic' src={process.env.PUBLIC_URL+"/bio.jpg"} style={{ "margin": "auto"}} alt="icon"></img>
+      <h1 className="title">ChiHsuan Lee</h1>
+      <h2 className='title2'>An undergraduate student major in computer science.</h2>
+      <h3 className='content'> 🌱 I'm currently learning <strong>React , Vue, Node.js, Frontend Development, and Deutsch.</strong></h3>
+      <div className='imageContainer'>
+        <a href='mailto:hi@chihsuan-lee.dev'>
+          <img src='https://img.shields.io/badge/E--Mail-hi%40chiusuan--lee.dev-blue?style=for-the-badge&logo=Gmail ' alt='E-Mail: hi@chihsuan-lee.dev' ></img>
         </a>
-      </header>
+
+        <a href='https://code.chihsuan-lee.dev'>
+          <img src='https://img.shields.io/badge/GitHub-LeeCH--OeO-2ea44f?style=for-the-badge&logo=GitHub' alt='GitHub: LeeCH-OeO'></img>
+        </a>
+        <p></p>
+        <img src='https://walfiegif.files.wordpress.com/2020/11/out-transparent-53.gif?w=400' style={{'max-width': '100%'}} alt=""></img>
+      </div>
+      
+      
     </div>
   );
 }
