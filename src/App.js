8793822012dee5670fa2ef5be100210d73573aa2
leech <fairprice81000@gmail.com>
2022-04-23 03:29:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import './App.css'
+import "./App.css";
 function App() {
   return (
     <div>
-      <img className='pic' src={process.env.PUBLIC_URL+"/bio.jpg"} style={{ "margin": "auto"}} alt="icon"></img>
+      <img
+        className="pic"
+        src={process.env.PUBLIC_URL + "/bio.jpg"}
+        style={{ margin: "auto" }}
+        alt="icon"
+      ></img>
       <h1 className="title">ChiHsuan Lee</h1>
-      <h2 className='title2'>An undergraduate student major in computer science.</h2>
-      <h3 className='content'> 🌱 I'm currently learning <strong>React , Vue, JavaScript, Frontend Development and Deutsch.</strong></h3>
-      <div className='imageContainer'>
-        <a href='mailto:hi@chihsuan-lee.dev'>
-          <img src='https://img.shields.io/badge/E--Mail-hi%40chiusuan--lee.dev-blue?style=for-the-badge&logo=Gmail ' alt='E-Mail: hi@chihsuan-lee.dev' ></img>
+      <h2 className="title2">
+        An undergraduate student major in computer science.
+      </h2>
+      <h3 className="content">
+        {" "}
+        🌱 I'm currently learning{" "}
+        <strong>
+          React, Express, JavaScript, TypeScript, Web Development and Deutsch.
+        </strong>
+      </h3>
+      <div className="imageContainer">
+        <a href="mailto:hi@chihsuan-lee.dev">
+          <img
+            src="https://img.shields.io/badge/E--Mail-hi%40chiusuan--lee.dev-blue?style=for-the-badge&logo=Gmail "
+            alt="E-Mail: hi@chihsuan-lee.dev"
+          ></img>
         </a>
 
-        <a href='https://code.chihsuan-lee.dev'>
-          <img src='https://img.shields.io/badge/GitHub-LeeCH--OeO-2ea44f?style=for-the-badge&logo=GitHub' alt='GitHub: LeeCH-OeO'></img>
+        <a href="https://code.chihsuan-lee.dev">
+          <img
+            src="https://img.shields.io/badge/GitHub-LeeCH--OeO-2ea44f?style=for-the-badge&logo=GitHub"
+            alt="GitHub: LeeCH-OeO"
+          ></img>
         </a>
         <p></p>
-        <img src='https://walfiegif.files.wordpress.com/2020/11/out-transparent-53.gif' style={{'height':'40vh'}} alt=""></img>
+        <img
+          src="https://walfiegif.files.wordpress.com/2020/11/out-transparent-53.gif"
+          style={{ height: "40vh" }}
+          alt=""
+        ></img>
       </div>
-      
-      
     </div>
   );
 }
